5444e4589a93bc2f6b30d216dc2108572150991b
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2020-10-29 15:21:30 +0100

    fix typo

--- source/_data/site.js
+++ source/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   buildTime: new Date(),
-  baseUrl: "https://data.odis-berlin.de",
+  baseUrl: "https://daten.odis-berlin.de",
   s3BaseUrl: "https://tsb-opendata.s3.eu-central-1.amazonaws.com/",
   en: {
     metaTitle: "Data",
