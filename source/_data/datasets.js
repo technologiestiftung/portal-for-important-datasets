942f295185677df3872a7a869e50d6a5a4d6f9ef
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2020-10-29 16:59:10 +0100

    exclude archive dir on aws from build

--- source/_data/datasets.js
+++ source/_data/datasets.js
@@ -22,66 +22,68 @@ const getDatasets = () => {
         // get all objects in the folder
         return s3.listObjectsV2({ Bucket: bucket, Delimiter: "/", Prefix: obj.Prefix }).promise()
           .then((dataItems) => {
-            const dataset = {
-              folder: obj.Prefix,
-              meta: false,
-              hasPreview: false,
-              hasThumb: false,
-              hasGeojson: false,
-              geojson: '',
-              formats: []
-            };
+            if (obj.Prefix !== "0_archive/") {
+              const dataset = {
+                folder: obj.Prefix,
+                meta: false,
+                hasPreview: false,
+                hasThumb: false,
+                hasGeojson: false,
+                geojson: '',
+                formats: []
+              };
 
-            dataItems.Contents.forEach((dataItem) => {
-              switch(dataItem.Key.split("/")[1]) {
-                // yay meta file
-                case "meta.json":
-                  dataset.meta = true;
-                  break;
-                // wohooo preview image
-                case "preview.jpg":
-                  dataset.hasPreview = true;
-                  break;
-                // horray thumbnail for the list
-                case "thumb.jpg":
-                  dataset.hasThumb = true;
-                  break;
-                // the rest should be datafiles
-                default:
-                  // the folder itself also shows up here (Size === 0)
-                  if (dataItem.Size > 0) {
-                    dataset.formats.push({
-                      file: dataItem.Key,
-                      size: dataItem.Size,
-                      format: dataItem.Key.split("/")[1].split(".")[1]
-                    });
-                    if (dataItem.Key.indexOf(".geojson") >= 0 && dataItem.Key.indexOf("zip") == -1) {
-                      dataset.hasGeojson = dataItem.Key;
+              dataItems.Contents.forEach((dataItem) => {
+                switch(dataItem.Key.split("/")[1]) {
+                  // yay meta file
+                  case "meta.json":
+                    dataset.meta = true;
+                    break;
+                  // wohooo preview image
+                  case "preview.jpg":
+                    dataset.hasPreview = true;
+                    break;
+                  // horray thumbnail for the list
+                  case "thumb.jpg":
+                    dataset.hasThumb = true;
+                    break;
+                  // the rest should be datafiles
+                  default:
+                    // the folder itself also shows up here (Size === 0)
+                    if (dataItem.Size > 0) {
+                      dataset.formats.push({
+                        file: dataItem.Key,
+                        size: dataItem.Size,
+                        format: dataItem.Key.split("/")[1].split(".")[1]
+                      });
+                      if (dataItem.Key.indexOf(".geojson") >= 0 && dataItem.Key.indexOf("zip") == -1) {
+                        dataset.hasGeojson = dataItem.Key;
+                      }
                     }
-                  }
-                  break;
-              }
-            });
+                    break;
+                }
+              });
 
-            // if there is a meta file get it an merge into data set, otherwise return null
-            if (dataset.meta) {
-              const url = bucketUrl + obj.Prefix + "meta.json";
-              return fetch(url)
-                .then((response) => {
-                  if (response.ok) {
-                    return response.json();
-                  } else {
-                    throw Error(`Error fetching ${url}`)
-                  }
-                })
-                .then((json) => {
-                  dataset.meta = {
-                    meta: json
-                  };
-                  return dataset;
-                });
-            } else {
-              return Promise.resolve(null);
+              // if there is a meta file get it an merge into data set, otherwise return null
+              if (dataset.meta) {
+                const url = bucketUrl + obj.Prefix + "meta.json";
+                return fetch(url)
+                  .then((response) => {
+                    if (response.ok) {
+                      return response.json();
+                    } else {
+                      throw Error(`Error fetching ${url}`)
+                    }
+                  })
+                  .then((json) => {
+                    dataset.meta = {
+                      meta: json
+                    };
+                    return dataset;
+                  });
+              } else {
+                return Promise.resolve(null);
+              }
             }
           })
       }));
@@ -114,15 +116,17 @@ const getDatasets = () => {
     // })
     .then((objects) => {
       const pageList = [];
-      objects.filter((obj) => obj !== null)
+      objects.filter((obj) => obj !== null) 
       .forEach((obj) => {
         (["de", "en"]).forEach((lang) => {
-          const page = JSON.parse(JSON.stringify(obj));
-          page.lang = lang;
-          page.title = page.meta.meta[lang].title;
-          page.description = page.meta.meta[lang].description;
-          page.keywords = page.meta.meta[lang].keywords;
-          pageList.push(page);
+          if (obj !== undefined){
+            const page = JSON.parse(JSON.stringify(obj));
+            page.lang = lang;
+            page.title = page.meta.meta[lang].title;
+            page.description = page.meta.meta[lang].description;
+            page.keywords = page.meta.meta[lang].keywords;
+            pageList.push(page);
+           }
         });
       });
       return pageList;
@@ -130,7 +134,11 @@ const getDatasets = () => {
     .catch((err) => {
       throw err;
     });
-};
+
+
+    
+  }
+;
 
 module.exports = async function () {
   try {
